20326f4c32673b20f681dcb448db2a56b24b95ce
Álvaro Lemos <alvarolemos@gmail.com>
2017-09-23 18:40:59 -0300

    Added MLP file

---
+++ R/mlp.R
@@ -0,0 +1,41 @@
+passForward <- function(X, W) {
+  Xbiased <- addBias(X)
+  net <- Xbiased %*% t(W)
+  return(net)
+}
+
+
+addBias <- function(X) {
+  return(cbind(1, X))
+}
+
+
+removeBias <- function(W) {
+  matrix(W[, 2:ncol(W)], nrow=nrow(W), ncol=ncol(W)-1)
+}
+
+
+initializeRandomWeights <- function(inputSize, outputSize) {
+  randomValues <- runif(inputSize * outputSize)
+  return(matrix(randomValues, nrow=outputSize, ncol=inputSize))
+}
+
+
+sigmoid = function(x) {
+  return(1 / (1 + exp(-x)))
+}
+
+
+sigmoidDerivative = function(x) {
+  return(sigmoid(x) * (1 - sigmoid(x)))
+}
+
+
+computeCost <- function(targets, z) {
+  return(0.5 * sum(targets - z) ^ 2)
+}
+
+
+activate <- function(net) {
+  return(sigmoid(net))
+}
